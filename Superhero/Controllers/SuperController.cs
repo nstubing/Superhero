d0d9cf57153d410e53903b0ba9870552ee7c4898
nstubing <n.stubing886@gmail.com>
2018-07-24 11:42:57 -0500

    create, read, and update gets finished

--- Superhero/Controllers/SuperController.cs
+++ Superhero/Controllers/SuperController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Superhero.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,28 @@ namespace Superhero.Controllers
 {
     public class SuperController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: Super
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult Create()
+        {
+            ViewBag.SuperHeroID = new SelectList(db.Superheroes, "Name", "alterEgo", "superHeroAbility", "secondSuperHeroAbility", "catchphrase");
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            ViewBag.SuperHeroID = new SelectList(db.Superheroes);
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            ViewBag.SuperHeroID = new SelectList(db.Superheroes, "Name","alterEgo", "superHeroAbility", "secondSuperHeroAbility", "catchphrase");
+            return View();
+        }
+        public ActionLIst List()
     }
 }
