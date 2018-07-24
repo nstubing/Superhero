3b1ad5b640d452906f37047feb830019362b159b
nstubing <n.stubing886@gmail.com>
2018-07-24 10:49:44 -0500

    initial commit

---
+++ Superhero/Models/Superhero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Superhero.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public string Name { get; set; }
+        public string alterEgo { get; set; }
+        public string superHeroAbility { get; set; }
+        public string secondSuperHeroAbility { get; set; }
+        public string catchphrase { get; set; }
+    }
+}
